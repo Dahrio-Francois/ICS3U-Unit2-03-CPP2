76c52d337bbdbb760784af8feea0435a7c251886
Dahrio-Francois <dahrio.francois@mths.ca>
2022-03-03 00:48:50 +0000

    added circumference.cpp

---
+++ circumference.cpp
@@ -0,0 +1,26 @@
+// Copyright (c) 2022 Dahrio Francois All rights reserved
+//
+// Created by: Dahrio Francois
+// Created on: Feb 2022
+// This program calculates the circumference of a circle
+//    with user input
+
+#include <iostream>
+
+int main() {
+    // this function calculates the circumference of the circle
+    const double TAU = 6.28;
+    int radius;
+    double circumference;
+
+    // input
+    std::cout << "Enter the radius of the circle (mm): ";
+    std::cin >> radius;
+
+    // process
+    circumference = TAU*radius;
+
+    // output
+    std::cout << "" << std::endl;
+    std::cout << "Circumference is " << circumference << " mm²" << std::endl;
+}
